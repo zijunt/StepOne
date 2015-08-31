06a8cc7f1a3cd570531cf8a84d57ea7c69a6f9a8
Roy Tang <roy.tang@hootsuite.com>
2015-08-31 00:17:36 -0700

    12:17

--- app/src/main/java/com/learning/stepone/MainActivity.java
+++ app/src/main/java/com/learning/stepone/MainActivity.java
@@ -2,15 +2,21 @@ package com.learning.stepone;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.learning.stepone.model.Photo;
 import com.learning.stepone.model.PhotoPage;
 import com.learning.stepone.model.TestData;
+
+import java.util.List;
 
 /**
  * The main screen will show:
@@ -21,19 +27,43 @@ import com.learning.stepone.model.TestData;
 public class MainActivity extends AppCompatActivity {
 
     private ListView mListView;
+    private Button emptyButton;
+    List<Photo> photoList;
+    PhotoListAdapter photoListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mListView = (ListView)findViewById(R.id.listView);
+        mListView.setEmptyView(findViewById(R.id.emptyInfoLayout));
+
+
+
+        //botton
+        emptyButton = (Button) findViewById(R.id.Button01);
+
+
         Gson gson = new Gson();
         PhotoPage flickrPhotos = gson.fromJson(TestData.testData, PhotoPage.class);
+        photoListAdapter = new PhotoListAdapter(this, flickrPhotos.getPhoto());
+        Log.i("out",flickrPhotos.getPhoto().toString());
+
+        emptyButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                mListView.setAdapter(photoListAdapter);
+                photoListAdapter.notifyDataSetChanged();
+            }
+        });
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // go to the photo details screen
+                Toast.makeText(MainActivity.this, photoList.get(position).getPhotoUrlThumbNail(), Toast.LENGTH_LONG)
+                        .show();
             }
         });
     }
--- app/src/main/java/com/learning/stepone/PhotoListAdapter.java
+++ app/src/main/java/com/learning/stepone/PhotoListAdapter.java
@@ -1,29 +1,39 @@
 package com.learning.stepone;
 
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.learning.stepone.model.Photo;
 
+import java.net.URL;
 import java.util.List;
 
 public class PhotoListAdapter extends BaseAdapter {
+    private List<Photo> mPhoto;
+    private final Context context;
 
-    public PhotoListAdapter(List<Photo> photos) {
+    public PhotoListAdapter(Context context, List<Photo> photos) {
         // TODO fill in
+        this.context = context;
+        mPhoto = photos;
     }
 
     @Override
     public int getCount() {
         // TODO fill in
-        return 0;
+        return mPhoto.size();
     }
 
     @Override
     public Object getItem(int position) {
         // TODO fill in
-        return null;
+        return mPhoto.get(position);
     }
 
     @Override
@@ -32,15 +42,32 @@ public class PhotoListAdapter extends BaseAdapter {
         return 0;
     }
 
+    public class Holder
+    {
+        TextView title;
+        ImageView img;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // TODO fill in
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
         View view = null;
         if (convertView == null) {
             // inflate new view
+            convertView = inflater.inflate(R.layout.photo_row, parent, false);
         }
 
         // bind data to view
+        TextView textView = (TextView) convertView.findViewById(R.id.photoTitle);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.photoImageView);
+        textView.setText(mPhoto.get(position).getTitle());
+        Uri myUri = Uri.parse(mPhoto.get(position).getPhotoUrlThumbNail());
+        imageView.setImageURI(myUri);
+
 
 
         return convertView;
